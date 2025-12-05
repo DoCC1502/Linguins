d50672085c843d5df400aee9fde78e8b70ea320e
bgenc <bgenc@student.tgm.ac.at>
2025-12-05 10:31:27 +0100

    Projekt Klassen hinzugefügt, Dependencies hinzugefgt und Framework eingesetzt

---
+++ LinguinsSpringBoot/src/main/java/at/ac/tgm/linguinsspringboot/converter/LinguinsMapper.java
@@ -0,0 +1,4 @@
+package at.ac.tgm.linguinsspringboot.converter;
+
+public class LinguinsMapper {
+}
---
+++ LinguinsSpringBoot/src/main/java/at/ac/tgm/linguinsspringboot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package at.ac.tgm.linguinsspringboot.exception;
+
+public class GlobalExceptionHandler {
+}
